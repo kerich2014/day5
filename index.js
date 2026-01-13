0c76b3c2f81c707d187f5411e0d1b00e1ab3676c
Kirill Zolotaryov <252222223a@gmail.com>
2026-01-13 18:45:03 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const price = 100; // цена товара
+const quantity = 3; // количество
+const discount = quantity > 2 ? 0.1 : 0; // скидка 10% если >2
+const total = price * quantity * (1 - discount);
+if(total > -1){
+    console.log(`Итого: ${total} руб.`);
+}
